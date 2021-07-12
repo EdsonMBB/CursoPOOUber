7261fe45b4c6133b2a7bd5fc9c86bc1cda9b9f57
Edson Miguel Bahena Barrios <bahena.edson@gmail.com>
2021-07-11 19:49:52 -0500

    Agregamos Encapsulamiento y polimorfismo

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 class Main {
     public static void main(String[] args) {
-        Car car  = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
-        car.passenger = 4; 
-       car.printDataCar();
+        UberX uberX  = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Sonic");
+        uberX.setPassenger(2);
+        uberX.printDataCar();
+
+        UberVan uberV = new UberVan("FGH324", new Account("Andres Herrera", "AND123"));
+        uberV.setPassenger(5);
+        uberV.printDataCar();
+
     }
+
+    
 }
